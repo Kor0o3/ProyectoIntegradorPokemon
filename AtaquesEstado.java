83b7f7fb43ca27521213751507861e27c3bfe5e0
Raúl Martín Atencia <146926126+Kor0o3@users.noreply.github.com>
2026-01-28 19:47:02 +0100

    Add files via upload

---
+++ AtaquesEstado.java
@@ -0,0 +1,10 @@
+package Programa;
+
+public abstract class AtaquesEstado extends Ataque{
+
+	public AtaquesEstado(String nombre, int potencia, int precision, int pp) {
+		super(nombre, 0, precision, pp);
+		// TODO Auto-generated constructor stub
+	}
+
+}
