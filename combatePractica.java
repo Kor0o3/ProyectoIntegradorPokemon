0dd30a8664bc944c24dba83fe1615da965f7321c
Raúl Martín Atencia <146926126+Kor0o3@users.noreply.github.com>
2026-01-29 09:55:08 +0100

    Add files via upload
    Actualización del codigo y optimización del main

--- combatePractica.java
+++ combatePractica.java
@@ -8,12 +8,11 @@ public class combatePractica {
 		boolean pokemonDerrotado = false;
 		int masVeloz = 0, opcionP1 = 0, opcionP2 = 0, cont = 1;
 
-		Pokemon pokemon1 = new Pokemon("Pikachu", 55, 55, 20, 100, 10, 120);
-		pokemon1.agregarAtaque(new AtaqueTipo("Ataque Rapido", "normal", 80, 60, 30, true));
-		pokemon1.agregarAtaque(new AtaqueTipo("Portazo", "cuerpo", 80, 75, 20, false));
+		Pokemon pokemon1 = new Pokemon("Pikachu", 45, 55, 20, 100, 10, 120);
+		pokemon1.agregarAtaque(new AtaqueTipo("Ataque Rapido", "normal", 30, 60, 15, true));
+		pokemon1.agregarAtaque(new AtaqueTipo("Portazo", "normal", 80, 75, 20, false));
 		pokemon1.agregarAtaque(new AtaqueTipo("Rayo", "especial", 90, 100, 15, false));
 		pokemon1.agregarAtaque(new AtaqueTipo("Trueno", "especial", 100, 45, 15, false));
-		pokemon1.agregarAtaque(new AtaqueTipo("Impactrueno", "especial", 40, 100, 15, false));
 
 		Pokemon pokemon2 = new Pokemon("Charmander", "Clipper", 50, 85, 20, 65, 25, 120);
 		pokemon2.agregarAtaque(new AtaqueTipo("Placaje", "normal", 80, 100, 30, false));
@@ -22,12 +21,12 @@ public class combatePractica {
 		if (pokemon1.esValidoParaCombate() && pokemon2.esValidoParaCombate()) {
 			mostrarPokemons(pokemon1, pokemon2);
 			do {
+				// Pokemon1 ataca primero
 				if (pokemon1.getVelocidad() > pokemon2.getVelocidad() || masVeloz == 1) {
-					// Pokemon1 ataca primero
+					// Primera parte escoger los movimientos
 					if (!pokemonDerrotado && pokemon1.getPuntosVidaActuales() > 0
 							&& pokemon2.getPuntosVidaActuales() > 0) {
 						System.out.println("Ronda " + cont);
-
 						System.out.println("——————————————————————————————————");
 						System.out.println("Turno de " + pokemon1.validarNombre());
 						System.out.println("——————————————————————————————————");
@@ -35,7 +34,7 @@ public class combatePractica {
 						do {
 							System.out.print("Que ataque hara " + pokemon1.validarNombre() + " : ");
 							opcionP1 = sc.nextInt();
-						} while (opcionP1 < 1 || opcionP1 > 4);
+						} while (opcionP1 < 1 || opcionP1 > 4 || !pokemon1.puedesUsarAtaque(opcionP1));
 
 						System.out.println("——————————————————————————————————");
 						System.out.println("Turno de " + pokemon2.validarNombre());
@@ -44,9 +43,10 @@ public class combatePractica {
 						do {
 							System.out.print("Que ataque hara " + pokemon2.validarNombre() + " : ");
 							opcionP2 = sc.nextInt();
-						} while (opcionP2 < 1 || opcionP2 > 4);
+						} while (opcionP2 < 1 || opcionP2 > 4 || !pokemon2.puedesUsarAtaque(opcionP2));
 					}
 
+					// Segunda parte atacan
 					if (pokemon1.getPuntosVidaActuales() == 0) {
 						pokemonDerrotado = true;
 						System.out.println(pokemon1.validarNombre() + " ha sido derrotado");
@@ -54,42 +54,18 @@ public class combatePractica {
 						pokemonDerrotado = true;
 						System.out.println(pokemon2.validarNombre() + " ha sido derrotado");
 					} else if (!pokemonDerrotado && pokemon1.AtacaConPrioridad(opcionP1)) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon1, pokemon2, opcionP1, opcionP2);
 					} else if (!pokemonDerrotado && pokemon2.AtacaConPrioridad(opcionP2)) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon2, pokemon1, opcionP2, opcionP1);
 					} else if (!pokemonDerrotado) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon1, pokemon2, opcionP1, opcionP2);
 					}
 				} else if (pokemon2.getVelocidad() > pokemon1.getVelocidad() || masVeloz == 2) {
 					// Pokemon2 ataca primero
 					if (!pokemonDerrotado && pokemon1.getPuntosVidaActuales() > 0
 							&& pokemon2.getPuntosVidaActuales() > 0) {
+						// Primera parte escoger los movimientos
 						System.out.println("Ronda " + cont);
-
 						System.out.println("——————————————————————————————————");
 						System.out.println("Turno de " + pokemon2.validarNombre());
 						System.out.println("——————————————————————————————————");
@@ -97,7 +73,7 @@ public class combatePractica {
 						do {
 							System.out.print("Que ataque hara " + pokemon2.validarNombre() + " : ");
 							opcionP2 = sc.nextInt();
-						} while (opcionP2 < 1 || opcionP2 > 4);
+						} while (opcionP2 < 1 || opcionP2 > 4 || !pokemon2.puedesUsarAtaque(opcionP2));
 
 						System.out.println("——————————————————————————————————");
 						System.out.println("Turno de " + pokemon1.validarNombre());
@@ -106,9 +82,9 @@ public class combatePractica {
 						do {
 							System.out.print("Que ataque hara " + pokemon1.validarNombre() + " : ");
 							opcionP1 = sc.nextInt();
-						} while (opcionP1 < 1 || opcionP1 > 4);
+						} while (opcionP1 < 1 || opcionP1 > 4 || !pokemon1.puedesUsarAtaque(opcionP1));
 					}
-
+					// segunda parte atacan
 					if (pokemon1.getPuntosVidaActuales() == 0) {
 						pokemonDerrotado = true;
 						System.out.println(pokemon1.validarNombre() + " ha sido derrotado");
@@ -116,36 +92,11 @@ public class combatePractica {
 						pokemonDerrotado = true;
 						System.out.println(pokemon2.validarNombre() + " ha sido derrotado");
 					} else if (!pokemonDerrotado && pokemon2.AtacaConPrioridad(opcionP2)) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon2, pokemon1, opcionP2, opcionP1);
 					} else if (!pokemonDerrotado && pokemon1.AtacaConPrioridad(opcionP1)) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon1, pokemon2, opcionP1, opcionP2);
 					} else if (!pokemonDerrotado) {
-						System.out.println("——————————————————————————————————");
-						if (pokemon2.getPuntosVidaActuales() > 0) {
-							pokemon2.atacar(pokemon1, opcionP2);
-						}
-						if (pokemon1.getPuntosVidaActuales() > 0) {
-							pokemon1.atacar(pokemon2, opcionP1);
-						}
-						mostrarPokemons(pokemon1, pokemon2);
-						System.out.println("——————————————————————————————————");
+						combatePokemon(pokemon2, pokemon1, opcionP2, opcionP1);
 					}
 				} else {// En caso de empate inicializamos masVeloz con un numero aleatorio que decida
 						// quien ataca primero
@@ -158,8 +109,21 @@ public class combatePractica {
 		}
 	}
 
+	// Metodos para optimizar un poco el main
 	public static void mostrarPokemons(Pokemon pokemon1, Pokemon pokemon2) {
 		pokemon1.mostrarEstadoDelPokemon();
 		pokemon2.mostrarEstadoDelPokemon();
+	}
+
+	public static void combatePokemon(Pokemon pAtacante, Pokemon pDefensor, int oAtacante, int oDefensor) {
+		System.out.println("——————————————————————————————————");
+		if (pAtacante.getPuntosVidaActuales() > 0) {
+			pAtacante.atacar(pDefensor, oAtacante);
+		}
+		if (pDefensor.getPuntosVidaActuales() > 0) {
+			pDefensor.atacar(pAtacante, oDefensor);
+		}
+		mostrarPokemons(pAtacante, pDefensor);
+		System.out.println("——————————————————————————————————");
 	}
 }
