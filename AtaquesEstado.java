94a10d2ec4329ef66cad694a98b027625d775f77
Raúl Martín Atencia <146926126+Kor0o3@users.noreply.github.com>
2026-03-11 20:19:37 +0100

    Actualizacion AtaquesEstado

--- AtaquesEstado.java
+++ AtaquesEstado.java
@@ -2,8 +2,8 @@ package ProyectoIntegradorPokemon;
 
 public abstract class AtaquesEstado extends Ataque{
 
-	public AtaquesEstado(String nombre, String tipo, int potencia, int precision, int pp, boolean prioridad) {
-		super(nombre, tipo, 0, precision, pp, prioridad);
+	public AtaquesEstado(String nombre, int precision, int pp, boolean prioridad) {
+		super(nombre, "Estado", 0, precision, pp, prioridad);
 		// TODO Auto-generated constructor stub
 	}
 }
