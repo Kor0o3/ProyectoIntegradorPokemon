d78e220cf1b2fc9dee5dcc12c411f5e4dabf583c
Manurf04 <MROMFER1102@G.EDUCAAND.ES>
2026-03-11 20:43:38 +0100

    Update AtaqueRetroceso.java

--- AtaqueRetroceso.java
+++ AtaqueRetroceso.java
@@ -1,37 +1,52 @@
-package Programa;
+package ProyectoIntegradorPokemon;
 
-public class AtaqueRetroceso extends Ataque {
+public class AtaqueRetroceso extends AtaqueTipo {
 	
 
-	public AtaqueRetroceso(String nombre, String tipo, int potencia, int precision, int pp, boolean prioridad) { //ESTE TIPO DE ATAQUE RESTA VIDA AL POKEMON QUE LO USA, LA VIDA RESTADA SE BASA EN UN RNG
+	public AtaqueRetroceso(String nombre, String tipo, int potencia, int precision, int pp, boolean prioridad) { 
 		super(nombre, tipo, potencia, precision, pp, prioridad);
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
 	public void utilizar(Pokemon pAtacante, Pokemon pEnemigo) {
-		int rng = (int) (Math.random() * 100);
+		int danio = 0;
 		if (tienePps()) {
-			if(rng>25) {
-				pEnemigo.reducirPuntosDeVida(this.potencia);
-				pAtacante.puntosVidaActuales-=pAtacante.puntosVidaActuales/2;
-				System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido por su ataque con retroceso!");
-			}else if(rng>65) {
-				pEnemigo.reducirPuntosDeVida(this.potencia);
-				pAtacante.puntosVidaActuales-=this.potencia/3;
-				System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido por su ataque con retroceso!");
+			ppActual--;
+			if (acierta()) {
+				
+				if (this.getTipo().equalsIgnoreCase("normal")) {
+					if(rng>25) {
+						pAtacante.reducirPuntosDeVida(pAtacante.puntosVidaActuales/2);
+						System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido por su ataque con retroceso!");
+					}else if(rng>65) {
+						
+						pAtacante.reducirPuntosDeVida(this.potencia/3) ;
+						System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido bastante por su ataque con retroceso!");
 
-			}else {
-				pEnemigo.reducirPuntosDeVida(this.potencia);
-				pAtacante.puntosVidaActuales-=this.potencia/4;
-				System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido por su ataque con retroceso!");
+					}else {
+						pAtacante.reducirPuntosDeVida(this.potencia/4); 
+						System.out.println("La salud de "+pAtacante.nombrePokemon+" se ha reducido moderadamente por su ataque con retroceso!");
+					}
+					
+
+					danio = calcularDanio(pAtacante.getAtaqueFisico(), pEnemigo.getDefensaFisica());
+				} else if (this.getTipo().equalsIgnoreCase("especial")) {
+					danio = calcularDanio(pAtacante.getAtaqueEspecial(), pEnemigo.getDefensaEspecial());
+				}
+				pEnemigo.reducirPuntosDeVida(danio);
+				if (esCritico()) {
+					System.out.println(pAtacante.validarNombre() + " ha usado " + getNombre() + ". Golpe critico!");
+				} else {
+					System.out.println(pAtacante.validarNombre() + " ha usado " + getNombre());
+				}
+			} else {
+				System.out.println(pAtacante.validarNombre() + " ha usado " + getNombre() + " pero falló...");
 			}
-
 		} else {
-			System.out.println(getNombre() + " No tiene PP");
+			System.out.println(getNombre() + " no tiene PP");
 		}
-
 	}
+	//ESTE TIPO DE ATAQUE RESTA VIDA AL POKEMON QUE LO USA, LA VIDA RESTADA SE BASA EN UN RNG
+	
 
 	
 
