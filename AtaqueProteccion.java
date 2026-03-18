52710c20a97c59bb05750f661c999f6d2d17a17d
Arxb900 <arxb900@gmail.com>
2026-03-18 19:55:44 +0100

    Add files via upload

--- AtaqueProteccion.java
+++ AtaqueProteccion.java
@@ -4,30 +4,29 @@ public class AtaqueProteccion extends AtaquesEstado {
 
 		static int contador = 0;
 	
-		public AtaqueProteccion(String nombre, String tipo, int precision, int pp, boolean prioridad) {
-			super(nombre, tipo, precision, pp, prioridad);
+		public AtaqueProteccion(String nombre, String tipo, int precision, int pp, boolean prioridad,Tipo.Tipos tipoAtaque) {
+			super(nombre, tipo, precision, pp, prioridad, tipoAtaque);
 			// TODO Auto-generated constructor stub
 		}
 		
 		@Override
 		public void utilizar(Pokemon pAtacante, Pokemon pEnemigo) {
-
-			int puntosDefensa = pAtacante.getDefensaFisica();
+			
+			int precision = 100;
+			int precisionred = precision/2;
+			
 			
 			if(tienePps()) {
 				ppActual--;
 				if(acierta()) {
-					pAtacante.getDefensaFisica();
-					System.out.println(pAtacante.validarNombre() + "uso" + getNombre());
-					pAtacante.setDefensaFisica(+puntosDefensa);
+					System.out.println(pAtacante.validarNombre() + "ha usado " + getNombre());
+					System.out.println("La precision de " + pAtacante.validarNombre() + " es de " + precisionred);
 					contador++;
-					System.out.println("La defensa de" + pAtacante.validarNombre() + "subio");
-					
+				} else {
+					System.out.println(pAtacante.validarNombre() + " ha usado " + getNombre() + " pero falló...");
 					
 				}
-				
 			}
-			
-			
-		}
+				
+		}	
 	}
